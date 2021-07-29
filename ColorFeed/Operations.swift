96d001feb08a299ea2a656e98874927c7903ae0f
Syler Ip <sylerip@yahoo.com>
2021-07-29 18:53:16 +0800

    refined comment for Operations.swift

--- ColorFeed/Operations.swift
+++ ColorFeed/Operations.swift
@@ -55,7 +55,7 @@ class CoreMotionOperation: Operation {
                                                                         // }
 //                                                                        print(motionActivity)
                                                                         //(i<motionActivities!.count ? motionActivities![i+1].startDate:Date()) > motionActivities![i].startDate.addingTimeInterval(self.leadtime)
-                                                                        //leadtime is the time for classifing if the activity is valid and satisfy as checkout activity
+                                                                        //leadtime(activity duration) is the time for classifing if the activity is valid and fulfill the duration defined as a checkout activity
                                                                         print(i)
                                                                         if  ((i < motionActivities!.count-1 && motionActivities![i+1].startDate > motionActivities![i].startDate.addingTimeInterval(self.leadtime))||(i == motionActivities!.count-1 && Date() > motionActivities![i].startDate.addingTimeInterval(self.leadtime))) && motionActivities![i].startDate > q_time.addingTimeInterval(self.init_grace_period) {
                                                                             self.checkout(activity: motionActivities![i])
